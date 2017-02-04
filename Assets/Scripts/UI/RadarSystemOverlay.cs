9a5e0d2ed7e5408a6a7cdc9b271a483c2388ae45
Kittyfisto <kittyfisto87@gmail.com>
2017-02-04 15:06:23 +0100

    Added unit test tools

---
+++ Assets/Scripts/UI/RadarSystemOverlay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+	public class RadarSystemOverlay
+		: MonoBehaviour
+	{
+		// Use this for initialization
+		private void Start()
+		{
+		}
+
+		// Update is called once per frame
+		private void Update()
+		{
+		}
+	}
+}
