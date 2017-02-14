bf7936f874612b8b6cfc1ba545ad2a6b1365ed8c
Oleg Abrashchov <oleg.abr@gmail.com>
2017-02-14 23:14:40 +0100

    close behaviour added

---
+++ Assets/Scripts/UI/MainMenu/CloseBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.UI.MainMenu
+{
+	public class CloseBehaviour : MonoBehaviour
+	{
+		public void OnCloseButton()
+		{
+			Application.Quit();
+		}
+
+		void Update()
+		{
+			if (Input.GetKey(KeyCode.Escape))
+			{
+				Application.Quit();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/UI/MainMenu/LoadSceneBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.UI.MainMenu
+{
+	public class LoadSceneBehaviour : MonoBehaviour
+	{
+		public void OnCloseButton()
+		{
+			SceneManager.LoadScene("Chapters/Demo", LoadSceneMode.Additive);
+		}
+	}
+}
