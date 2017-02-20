daa3f1abf5bcfaff62fd920838c999af262a9fe5
Kittyfisto <kittyfisto87@gmail.com>
2017-02-20 21:10:26 +0100

    Added exit dialog to intro #14

---
+++ Assets/Scripts/UI/QuitGameComponent.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.UI
+{
+	public class QuitGameComponent
+		: MonoBehaviour
+	{
+		public GameObject QuitDialog;
+
+		// Update is called once per frame
+		void Update ()
+		{
+			if (Input.GetKeyDown(KeyCode.Escape))
+			{
+				ToggleVisibility();
+			}
+		}
+
+		private void ToggleVisibility()
+		{
+			QuitDialog.SetActive(!QuitDialog.activeInHierarchy);
+		}
+
+		public void OnQuit()
+		{
+			SceneManager.LoadScene("Scenes/MainMenu", LoadSceneMode.Single);
+		}
+
+		public void OnContinue()
+		{
+			QuitDialog.SetActive(false);
+		}
+	}
+}
