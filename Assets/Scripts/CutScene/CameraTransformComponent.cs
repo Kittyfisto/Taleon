34e9542b428b209dde0076326008417c960e3989
Kittyfisto <kittyfisto87@gmail.com>
2017-02-22 23:06:28 +0100

    Introduced 'CameraTransformComponent' #21

---
+++ Assets/Scripts/CutScene/CameraTransformComponent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.CutScene
+{
+	public sealed class CameraTransformComponent
+		: MonoBehaviour
+	{
+		public float DisplayLength = 10;
+
+		private void Update()
+		{
+			Debug.DrawRay(transform.position, transform.forward*DisplayLength, Color.red);
+		}
+	}
+}
