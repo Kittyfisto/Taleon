0dbf0e2419fa0d41506428433da00d647f8615f9
Kittyfisto <kittyfisto87@gmail.com>
2017-03-06 18:46:33 +0100

    Introduced camera cut action

---
+++ Assets/Scripts/CutScene/CameraCutAction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.CutScene
+{
+	public sealed class CameraCutAction
+		: ICutSceneAction
+	{
+		private readonly GameObject _anchor;
+		private readonly float _length;
+
+		public CameraCutAction(GameObject anchor, float length)
+		{
+			_anchor = anchor;
+			_length = length;
+		}
+
+		public float Length
+		{
+			get { return _length; }
+		}
+	}
+}
--- Assets/Scripts/CutScene/CutSceneController.cs
+++ Assets/Scripts/CutScene/CutSceneController.cs
@@ -9,10 +9,97 @@ namespace Assets.Scripts.CutScene
 	public sealed class CutSceneController
 		: MonoBehaviour
 	{
-		private ICutSceneAction[] _lines;
-		private int _currentLineIndex;
-		private float _currentLineElapsed;
-		private float _length;
+		/// <summary>
+		/// Responsible for executing a list of serial actions where each action
+		/// is executed for a certain amount of time.
+		/// </summary>
+		private sealed class CutSceneActionList
+		{
+			private readonly ICutSceneAction[] _lines;
+			private int _currentLineIndex;
+			private float _currentLineElapsed;
+			private readonly float _length;
+			private readonly Text _textBox;
+
+			public CutSceneActionList(Text textBox, IEnumerable<ICutSceneAction> actions)
+			{
+				_textBox = textBox;
+				_lines = actions.ToArray();
+				_currentLineIndex = -1;
+				_length = _lines.Sum(x => x.Length);
+
+				ExecuteNextAction();
+			}
+
+			public float Length
+			{
+				get { return _length; }
+			}
+
+			public int Count
+			{
+				get { return _lines.Length; }
+			}
+
+			public void Update()
+			{
+				var currentLine = GetCurrentAction();
+				if (currentLine == null)
+					return;
+
+				if (_currentLineElapsed > currentLine.Length)
+				{
+					ExecuteNextAction();
+				}
+				else
+				{
+					_currentLineElapsed += Time.deltaTime;
+				}
+			}
+
+			private void ExecuteNextAction()
+			{
+				++_currentLineIndex;
+				_currentLineElapsed = 0;
+
+				var currentAction = GetCurrentAction();
+				if (currentAction == null)
+					return;
+
+				Execute(currentAction);
+			}
+
+			private void Execute(ICutSceneAction currentAction)
+			{
+				var dialog = currentAction as DialogAction;
+				if (dialog != null)
+					_textBox.text = dialog.ToString();
+				else
+					_textBox.text = null;
+			}
+
+			public bool IsFinished
+			{
+				get
+				{
+					if (_lines == null || _currentLineIndex < 0 || _currentLineIndex >= _lines.Length)
+						return true;
+
+					return false;
+				}
+			}
+
+			private ICutSceneAction GetCurrentAction()
+			{
+				if (IsFinished)
+					return null;
+
+				var currentLine = _lines[_currentLineIndex];
+				return currentLine;
+			}
+		}
+
+		private CutSceneActionList[] _actionLists;
 
 		/// <summary>
 		/// The text component that shall display the dialog.
@@ -21,19 +108,23 @@ namespace Assets.Scripts.CutScene
 
 		private QuitGameComponent _quitDialog;
 
-		public void Play(IEnumerable<ICutSceneAction> actions)
+		public void Play(params IEnumerable<ICutSceneAction>[] actions)
 		{
 			gameObject.SetActive(true);
 
 			_quitDialog = FindObjectOfType<QuitGameComponent>();
 
-			_lines = actions.ToArray();
-			_currentLineIndex = -1;
-			_length = _lines.Sum(x => x.Length);
+			_actionLists = new CutSceneActionList[actions.Length];
+			var length = 0f;
+			int count = 0;
+			for (int i = 0; i < actions.Length; ++i)
+			{
+				_actionLists[i] = new CutSceneActionList(TextBox, actions[i]);
+				length = Mathf.Max(length, _actionLists[i].Length);
+				count += _actionLists[i].Count;
+			}
 
-			Debug.LogFormat("Starting cut scene {0}seconds, {1} actions", _length, _lines.Length);
-
-			ExecuteNextAction();
+			Debug.LogFormat("Starting cut scene {0}seconds, {1} actions", length, count);
 		}
 
 		private void Update()
@@ -42,59 +133,13 @@ namespace Assets.Scripts.CutScene
 			if (_quitDialog.IsShowingQuitDIalog)
 				return;
 
-			var currentLine = GetCurrentAction();
-			if (currentLine == null)
+			if (_actionLists == null)
 				return;
 
-			if (_currentLineElapsed > currentLine.Length)
+			foreach (var actionList in _actionLists)
 			{
-				ExecuteNextAction();
+				actionList.Update();
 			}
-			else
-			{
-				_currentLineElapsed += Time.deltaTime;
-			}
-		}
-
-		private void ExecuteNextAction()
-		{
-			++_currentLineIndex;
-			_currentLineElapsed = 0;
-
-			var currentAction = GetCurrentAction();
-			if (currentAction == null)
-				return;
-
-			Execute(currentAction);
-		}
-
-		private void Execute(ICutSceneAction currentAction)
-		{
-			var dialog = currentAction as DialogAction;
-			if (dialog != null)
-				TextBox.text = dialog.ToString();
-			else
-				TextBox.text = null;
-		}
-
-		public bool IsFinished
-		{
-			get
-			{
-				if (_lines == null || _currentLineIndex < 0 || _currentLineIndex >= _lines.Length)
-					return true;
-
-				return false;
-			}
-		}
-
-		private ICutSceneAction GetCurrentAction()
-		{
-			if (IsFinished)
-				return null;
-
-			var currentLine = _lines[_currentLineIndex];
-			return currentLine;
 		}
 	}
 }
